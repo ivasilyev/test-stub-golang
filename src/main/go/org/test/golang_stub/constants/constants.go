88b8f95dbc0a875ed69fdeb1c88fa363f381c1d1
ivasilyev <u0412u0418u042e@gmail.com>
2023-09-27 15:13:30 +0300

    Added static package to keep common constants

---
+++ src/main/go/org/test/golang_stub/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const CSS_DIR = "../../../../resources/static/styles/"
+const JS_DIR = "../../../../resources/static/scripts/"
+const UI_TEMPLATE_FILE = "../../../../resources/static/templates/ui.html"
+
+const API_GET_SUFFIX = "/api/get-delay"
+const API_SET_SUFFIX = "/api/set-delay"
+const API_RESET_SUFFIX = "/api/reset-delay"
+const UI_SUFFIX = "/ui"
