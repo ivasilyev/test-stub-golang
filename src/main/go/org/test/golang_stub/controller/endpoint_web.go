6465628b13db209643194ca55ea5b34a49708276
ivasilyev <u0412u0418u042e@gmail.com>
2023-09-27 15:15:19 +0300

    Refactored Web UI endpoint processing

---
+++ src/main/go/org/test/golang_stub/controller/endpoint_web.go
@@ -0,0 +1,27 @@
+package controller
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"src/main/go/org/test/golang_stub/main.go/src/main/go/org/test/golang_stub/constants"
+)
+
+func renderTemplate(responseWriter http.ResponseWriter, templateData TemplateData) {
+	tmpl, err := template.ParseFiles(constants.UI_TEMPLATE_FILE)
+	if err != nil {
+		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(responseWriter, templateData)
+}
+
+func endPointUiHandler(responseWriter http.ResponseWriter, requests *http.Request) {
+	renderTemplate(responseWriter, TemplateData{
+		EndpointName: ENDPOINT_NAME,
+	})
+}
+
+func HandleWebEndpoint() {
+	http.HandleFunc(fmt.Sprintf("/%s%s", ENDPOINT_NAME, constants.UI_SUFFIX), endPointUiHandler)
+}
